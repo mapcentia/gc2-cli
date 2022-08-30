9fc960f6d76abedf5c1a0ff6e560bbb4caffa290
Martin Høgh <mh@mapcentia.com>
2022-08-30 08:50:20 +0200

    Help text

--- src/commands/sql.ts
+++ src/commands/sql.ts
@@ -9,7 +9,7 @@ import fetch from 'node-fetch'
 import {User} from '../interfaces/User'
 
 export default class Sql extends Command {
-  static description = 'Sign in to GC2. You can set the connect options beforehand using the `connect` command. Providing the password on the commandline is considered insecure. It\'s better to be prompt for the password'
+  static description = 'Run SQL statements'
   static flags = {
     help: flags.help({char: 'h'}),
   }
