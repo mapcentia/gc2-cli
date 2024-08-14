64d319ef25d57410962e734786db6b33dcb6a289
Martin Høgh <mh@mapcentia.com>
2024-08-14 18:02:34 +0200

    Add commands for setting and getting table privileges
    Introduce 'set' and 'get' commands to handle user privileges on tables. The 'set' command updates privileges for a specified user, while the 'get' command retrieves current privileges.

---
+++ src/commands/privilege/get.ts
@@ -0,0 +1,33 @@
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2024 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
+
+import {Args, Command, Flags} from '@oclif/core'
+import chalk from 'chalk'
+import args from '../../common/base_args'
+import get from '../../util/get-response'
+import make from '../../util/make-request'
+import setSchema from '../../util/set-schema'
+
+let base_args = args
+let specific_args = {}
+
+export default class Get extends Command {
+  static description = 'Get privileges on table'
+
+  static flags = {
+    help: Flags.help({char: 'h'}),
+  }
+  static args = {...base_args, ...specific_args}
+
+  async run() {
+    let {args} = await this.parse(Get)
+    args = setSchema(args)
+    const response = await make('4', `schemas/${args.schema}/tables/${args.table}/privilege`, 'GET')
+    const data = await get(this, response, 200)
+    this.log(data)
+  }
+}
---
+++ src/commands/privilege/set.ts
@@ -0,0 +1,47 @@
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2024 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
+
+import {Args, Command, Flags} from '@oclif/core'
+import chalk from 'chalk'
+import args from '../../common/base_args'
+import get from '../../util/get-response'
+import make from '../../util/make-request'
+import setSchema from '../../util/set-schema'
+
+let base_args = args
+let specific_args = {
+  user: Args.string(
+    {
+      required: true,
+      description: 'Name of user',
+    },
+  ),
+  privileges: Args.string(
+    {
+      required: true,
+      description: 'Which privileges',
+    },
+  )
+}
+
+export default class Set extends Command {
+  static description = 'Set privileges on table'
+
+  static flags = {
+    help: Flags.help({char: 'h'}),
+  }
+
+  static args = {...base_args, ...specific_args}
+
+  async run() {
+    let {args} = await this.parse(Set)
+    args = setSchema(args)
+    const response = await make('4', `schemas/${args.schema}/tables/${args.table}/privilege`, 'PUT', {subuser: args.user, privileges: args.privileges})
+    await get(this, response, 200)
+    this.log(`Privileges update on ${chalk.green(args.table)}`)
+  }
+}
