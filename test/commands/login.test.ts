89f1afa6612c4eea782597167e85b04d8f898e5e
Martin Høgh <mh@mapcentia.com>
2024-12-04 17:03:57 +0100

    Standardize code descriptions and improve logging.
    Updated code to ensure consistent punctuation in descriptions across files and improved log messages for clarity. The changes include adding missing periods, refining privilege handling in privilege command, and enhancing SQL command options.

--- test/commands/login.test.ts
+++ test/commands/login.test.ts
@@ -3,7 +3,7 @@ import {expect, test} from '@oclif/test'
 describe('login', () => {
   test
     .stdout()
-    .command(['login', '--password', 'hawk2000'])
+    .command(['login', '--flow', 'password', '--user', 'mydb', '--password', 'hawk2000'])
     .it('runs login cmd', (ctx: { stdout: any }) => {
       console.log(ctx.stdout)
       expect(ctx.stdout).to.contain('success')
