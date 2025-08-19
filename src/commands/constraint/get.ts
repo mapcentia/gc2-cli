2f74a89c8e8a90baac375813c2f972729be820e5
Martin Høgh <mh@mapcentia.com>
2025-08-19 12:43:25 +0200

    Update version to 2025.8.0 and update references in documentation and commands
    Updated version references across the codebase and documentation from `2025.7.3` to `2025.8.0`. Adjusted `constraint get` and `index get` commands to refine output formatting and enhance schema handling for improved usability.

--- src/commands/constraint/get.ts
+++ src/commands/constraint/get.ts
@@ -1,24 +1,59 @@
 /**
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2024 MapCentia ApS
+ * @copyright  2013-2025 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  *
  */
 
-import {Command, Flags, ux} from '@oclif/core'
-import chalk from 'chalk'
+import {Args, Command, Flags} from '@oclif/core'
 import cli from 'cli-ux'
 import args from '../../common/base_args'
-import {tables} from '../../util/getters'
-import {schemasList, tableList} from '../../util/lists'
+import get from '../../util/get-response'
+import {constraintList, schemasList, tableList} from '../../util/lists'
+import make from '../../util/make-request'
 import setSchema from '../../util/set-schema'
 
 let base_args = args
-let specific_args = {}
+let specific_args = {
+  name: Args.string(
+    {
+      required: false,
+      description: 'Name of constraint.',
+    },
+  ),
+}
 
 export default class Get extends Command {
-  run(): Promise<any> {
-    return Promise.resolve(undefined)
+  static description = 'Get constraint details.'
+  static flags = {
+    help: Flags.help({char: 'h'}),
   }
+  static args = {...base_args, ...specific_args}
 
+  async run() {
+    let {args} = await this.parse(Get)
+
+    args = setSchema(args)
+    const schema = args?.schema || await schemasList()
+    const table = args?.table || await tableList(schema)
+    const name = args?.name || await constraintList(schema, table)
+
+    const response = await make('4', `schemas/${schema}/tables/${table}/constraints/${name}`, 'GET', null)
+    const res: any = await get(response, 200)
+
+    type row = { [key: string]: any }
+    const props: object[] = []
+    const rows: row = {prop: {}, value: {}}
+
+    props.push({prop: 'Name', value: res.name})
+    props.push({prop: 'Type', value: res.constraint})
+    if (res?.columns?.length) props.push({prop: 'Columns', value: res.columns.join(', ')})
+    if (res?.referenced_table) props.push({prop: 'Referenced table', value: res.referenced_table})
+    if (res?.referenced_columns?.length) props.push({prop: 'Referenced columns', value: res.referenced_columns.join(', ')})
+    if (res?.check) props.push({prop: 'Check', value: res.check})
+
+    cli.table(props, rows, {
+      printLine: this.log.bind(this)
+    })
+  }
 }
--- src/commands/index/get.ts
+++ src/commands/index/get.ts
@@ -1,24 +1,58 @@
 /**
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2024 MapCentia ApS
+ * @copyright  2013-2025 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  *
  */
 
-import {Command, Flags, ux} from '@oclif/core'
-import chalk from 'chalk'
+import {Args, Command, Flags} from '@oclif/core'
 import cli from 'cli-ux'
 import args from '../../common/base_args'
-import {tables} from '../../util/getters'
-import {schemasList, tableList} from '../../util/lists'
+import get from '../../util/get-response'
+import {schemasList, tableList, indexList} from '../../util/lists'
+import make from '../../util/make-request'
 import setSchema from '../../util/set-schema'
 
 let base_args = args
-let specific_args = {}
+let specific_args = {
+  name: Args.string(
+    {
+      required: false,
+      description: 'Name of index.',
+    },
+  ),
+}
 
 export default class Get extends Command {
-  run(): Promise<any> {
-    return Promise.resolve(undefined)
+  static description = 'Get index definition.'
+  static flags = {
+    help: Flags.help({char: 'h'}),
   }
+  static args = {...base_args, ...specific_args}
 
+  async run() {
+    let {args} = await this.parse(Get)
+
+    args = setSchema(args)
+    const schema = args?.schema || await schemasList()
+    const table = args?.table || await tableList(schema)
+    const name = args?.name || await indexList(schema, table)
+
+    const response = await make('4', `schemas/${schema}/tables/${table}/indices/${name}`, 'GET', null)
+    const res = await get(response, 200)
+
+    type row = {
+      [key: string]: any
+    }
+    const props: object[] = []
+    const rows: row = {prop: {}, value: {}}
+    props.push({prop: 'Name', value: res.name || name})
+    if (res?.method) props.push({prop: 'Method', value: res.method})
+    if (res?.columns) props.push({prop: 'Columns', value: (Array.isArray(res.columns) ? res.columns.join(', ') : res.columns)})
+    if (res?.unique !== undefined) props.push({prop: 'Unique', value: res.unique})
+
+    cli.table(props, rows, {
+      printLine: this.log.bind(this)
+    })
+  }
 }
